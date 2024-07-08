ab7108ec5d6736fc245a23262f56d10879e15c7d
Gokul Kannan <132426290+Gokul5001@users.noreply.github.com>
2024-07-08 15:13:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Router>
     <App />
-  </Router>
+  </Router>,
+  document.getElementById('root')
 );
